b87004189370556f896a5a45ff1bcacf4a41255d
Ujjawal Srivastava <aman.puggi@gmail.com>
2023-06-14 02:02:00 +0530

    Updated pagesize

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ import {
 } from "react-router-dom";
 
 export default class App extends Component {
+  pagesize  = 6;
   render() {
     return (
 
@@ -18,13 +19,13 @@ export default class App extends Component {
  <Router >
     <Navbar/>
       <Routes>
-      <Route  path="/general" element={<News key="general" pagesize={6} country="in" category="general"/> }/>
-      <Route  path="/health" element={<News key="health" pagesize={6} country="in" category="health"/> }/>
-      <Route  path="/business" element={<News key="business" pagesize={6} country="in" category="business"/> }/>
-      <Route  path="/science" element={<News key="science" pagesize={6} country="in" category="science"/> }/>
-      <Route  path="/sports" element={<News key="sports" pagesize={6} country="in" category="sports"/> }/>
-      <Route  path="/technology" element={<News key="technology" pagesize={6} country="in" category="technology"/> }/>
-      <Route  path="/entertainment" element={<News key="entertainment" pagesize={6} country="in" category="entertainment"/> }/>     
+      <Route  path="/general" element={<News key="general" pagesize={this.pagesize} country="in" category="general"/> }/>
+      <Route  path="/health" element={<News key="health" pagesize={this.pagesize} country="in" category="health"/> }/>
+      <Route  path="/business" element={<News key="business" pagesize={this.pagesize} country="in" category="business"/> }/>
+      <Route  path="/science" element={<News key="science" pagesize={this.pagesize} country="in" category="science"/> }/>
+      <Route  path="/sports" element={<News key="sports" pagesize={this.pagesize} country="in" category="sports"/> }/>
+      <Route  path="/technology" element={<News key="technology" pagesize={this.pagesize} country="in" category="technology"/> }/>
+      <Route  path="/entertainment" element={<News key="entertainment" pagesize={this.pagesize} country="in" category="entertainment"/> }/>     
     </Routes>
     </Router>
     </div>
